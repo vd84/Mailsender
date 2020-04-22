06d050fee5a6a7406fcb5428555beacb7ece3c5a
douglashammarstam <dogge20@hotmail.com>
2020-04-22 15:27:09 +0200

    init

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
-import java.sql.SQLException;
 
 public class Main {
 
     public static void main(String[] args) {
 
-
-        try {
             new GUI();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+
+
 
     }
 }
